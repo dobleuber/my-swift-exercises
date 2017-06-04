20eb9aae067a73d116346dd3283fff2fb062a0f2
Wbert Adrian Castro Vera <wbertc@gmail.com>
2017-06-04 08:12:54 -0500

    Project 2: Guess the flag game

---
+++ Project2/Project2/ViewController.swift
@@ -0,0 +1,80 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Wbert Castro on 3/06/17.
+//  Copyright © 2017 Wbert Castro. All rights reserved.
+//
+
+import UIKit
+import GameplayKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var labelScore: UILabel!
+    
+    var countries = [String]()
+    var score = 0
+    var correctAnswer = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        countries.append("estonia")
+        countries.append("france")
+        countries.append("germany")
+        countries.append("ireland")
+        countries.append("italy")
+        countries.append("monaco")
+        countries.append("nigeria")
+        countries.append("poland")
+        countries.append("russia")
+        countries.append("spain")
+        countries.append("uk")
+        countries.append("us")
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        askQuestion()
+    }
+    
+    func askQuestion(action: UIAlertAction! = nil) {
+        countries = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: countries) as! [String]
+        correctAnswer = GKRandomSource.sharedRandom().nextInt(upperBound: 3)
+        title = countries[correctAnswer].uppercased()
+        
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        if sender.tag == correctAnswer {
+            title = "Nice!!"
+            score += 1
+        } else {
+            title = "Oh, you are wrong"
+            score += -1
+        }
+        
+        labelScore.text = "Total Points: \(score)"
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(score).", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        present(ac, animated: true)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
