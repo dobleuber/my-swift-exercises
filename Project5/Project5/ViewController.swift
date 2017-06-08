9d82a8714c9ffa3bbbd3fe8bed410040e92e6cba
Wbert Adrian Castro Vera <wbertc@gmail.com>
2017-06-07 23:46:47 -0500

    Project5: customize the Word Scramble

--- Project5/Project5/ViewController.swift
+++ Project5/Project5/ViewController.swift
@@ -12,15 +12,19 @@ import GameplayKit
 class ViewController: UITableViewController {
     var allWords = [String]()
     var usedWords = [String]()
+    var currentWord = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
         
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(startGame))
         if let startWordsPath = Bundle.main.path(forResource: "start", ofType: "txt") {
             if let startWords = try? String(contentsOfFile: startWordsPath) {
                 allWords = startWords.components(separatedBy: "\n")
+            } else {
+                allWords = ["silkworm"]
             }
         } else {
             allWords = ["silkworm"]
@@ -40,8 +44,12 @@ class ViewController: UITableViewController {
     }
     
     func startGame() {
-        allWords = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: allWords) as! [String]
-        title = allWords[0]
+        if currentWord == 0 {
+            allWords = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: allWords) as! [String]
+        }
+        
+        title = allWords[currentWord]
+        currentWord += 1
         usedWords.removeAll(keepingCapacity: true)
         tableView.reloadData()
     }
@@ -75,24 +83,21 @@ class ViewController: UITableViewController {
                     
                     let indexPath = IndexPath(row: 0, section: 0)
                     tableView.insertRows(at: [indexPath], with: .automatic)
-                    
-                    return
                 } else {
                     errorTitle = "Word not recognised"
                     errorMessage = "You can't just make them up, you know"
+                    showErrorMessage(errorTitle: errorTitle, errorMessage: errorMessage)
                 }
             } else {
                 errorTitle = "Word used already"
                 errorMessage = "Be more original!"
+                showErrorMessage(errorTitle: errorTitle, errorMessage: errorMessage)
             }
         } else {
             errorTitle = "Word not possible"
             errorMessage = "You can't spell that word from '\(title!.lowercased())'"
+            showErrorMessage(errorTitle: errorTitle, errorMessage: errorMessage)
         }
-        
-        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "OK", style: .default))
-        present(ac, animated: true)
     }
     
     func isPossible(word: String) -> Bool {
@@ -110,15 +115,25 @@ class ViewController: UITableViewController {
     }
     
     func isOriginal(word: String) -> Bool {
-        return !usedWords.contains(word)
+        return word != title?.lowercased() && !usedWords.contains(word)
     }
     
     func isReal(word: String) -> Bool {
+        if word.characters.count <= 3 {
+            return false
+        }
+        
         let checker = UITextChecker()
         let range = NSMakeRange(0, word.utf16.count)
         let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
         
         return misspelledRange.location == NSNotFound
+    }
+    
+    func showErrorMessage(errorTitle: String, errorMessage: String) {
+        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
     }
     
     override func didReceiveMemoryWarning() {
