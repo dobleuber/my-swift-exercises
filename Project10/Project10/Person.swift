8cb92539af09d91baa4f83099ba6d211c6387895
Wbert Adrian Castro Vera <wbertc@gmail.com>
2017-06-28 05:03:39 -0500

    Project 10 -> Names to faces

---
+++ Project10/Project10/Person.swift
@@ -0,0 +1,20 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Wbert Castro on 28/06/17.
+//  Copyright © 2017 Wbert Castro. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+
+}
