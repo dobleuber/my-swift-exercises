62af7a506bea546a1f49ef817dcf4e9f0f9aadeb
Wbert Adrian Castro Vera <wbertc@gmail.com>
2017-07-04 05:20:04 -0500

    Project12: UserDefaults

---
+++ Project12/Project10/Person.swift
@@ -0,0 +1,30 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Wbert Castro on 28/06/17.
+//  Copyright © 2017 Wbert Castro. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject, NSCoding {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        name = aDecoder.decodeObject(forKey: "name") as! String
+        image = aDecoder.decodeObject(forKey: "image") as! String
+    }
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: "name")
+        aCoder.encode(image, forKey: "image")
+    }
+
+}
