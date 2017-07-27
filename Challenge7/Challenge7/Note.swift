fffa45a97c16c094d8492e20f985fdff830dafd0
Wbert Adrian Castro Vera <wbertc@gmail.com>
2017-07-27 06:10:56 -0500

    Project 18 -> Debugging

---
+++ Challenge7/Challenge7/Note.swift
@@ -0,0 +1,13 @@
+//
+//  Note.swift
+//  Challenge7
+//
+//  Created by Wbert Castro on 26/07/17.
+//  Copyright © 2017 Wbert Castro. All rights reserved.
+//
+
+import UIKit
+
+class Note: NSObject {
+
+}
---
+++ Project18/Project18/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Project18
+//
+//  Created by Wbert Castro on 24/07/17.
+//  Copyright © 2017 Wbert Castro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for i in 1 ... 100 {
+            print("Got number \(i)")
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
