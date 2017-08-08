660dad5b052837d302191e92b657fe150cbd93fa
Wbert Adrian Castro Vera <wbertc@gmail.com>
2017-08-07 21:01:48 -0500

    Project24 -> extensions

---
+++ Project24.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+extension Integer {
+    func squared() -> Self {
+        return self * self
+    }
+}
+
+var i = 3
+i.squared()
+
+let j: UInt = 5
+j.squared()
+
